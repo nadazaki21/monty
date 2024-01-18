6e0a88c59cf0ea9e48470f1e2f4945dab4705027
nadazkai21 <nadazaki019@gmail.com>
2024-01-19 00:03:27 +0200

    rotr , case of  nodes only

--- rotr.c
+++ rotr.c
@@ -23,13 +23,27 @@ void point_last_to_first(stack_t **top)
 */
 void monty_rotr(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp;
 	(void) stack;
 	(void) line_number;
 
 	if (main_stack.top == NULL ||
 	 ((main_stack.top != NULL) && (main_stack.top->next == NULL)))
 		return;
+	/* if they are 2 node only in the stack */
+	if ((main_stack.top != NULL) &&
+	 (main_stack.top->next != NULL) && (main_stack.top->next->next == NULL))
+	{
+		temp = main_stack.top;
 
+		main_stack.top = main_stack.top->next;
+
+		main_stack.top->next = temp;
+		main_stack.top->prev = NULL;
+
+		temp->prev = main_stack.top;
+		temp->next = NULL;
+		return;
+	}
 	point_last_to_first(&main_stack.top);
-
 }
